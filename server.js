e0de9fe9e0ed332af82a92df283618c78dc8f623
Jeffrey Johnson <frankp27@me.com>
2022-01-18 22:23:56 -0500

    worked on giving 404

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ const app = express();
 
 
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -67,6 +71,15 @@ app.get('/api/animals', (req, res) => {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }  
 });
 
 app.listen(PORT, () => {
